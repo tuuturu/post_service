d0c87044fbb68120ddf66610545642e22aa98c0a
Julius Pedersen <deifyed@gmail.com>
2020-03-15 12:36:10 +0100

    🐛 Fix gatekeeper url not being dynamic

--- source/auth.js
+++ source/auth.js
@@ -1,11 +1,8 @@
 const axios = require('axios')
 const log = require('./logging')
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-
 function extractToken(req) {
 	if (!req.headers.authorization) return null
-
 	const parts = req.headers.authorization.split(' ')
 
 	if (parts.length !== 2) return null
@@ -19,7 +16,9 @@ module.exports = async (req, res, next) => {
 	if (!access_token) return next()
 
 	try {
-		const { data } = await axios.get('https://localhost:4554/userinfo', {
+		const { data } = await axios.request({
+			url: `${process.env.GATEKEEPER_URL}/userinfo`,
+			method: 'get',
 			headers: {
 				Cookie: `access_token=${access_token}`
 			}
@@ -33,6 +32,7 @@ module.exports = async (req, res, next) => {
 			log.debug('Invalid token')
 
 		log.error('Something went wrong. No user attached')
+		log.error(error)
 
 		req.user = null
 		req.principal = null
