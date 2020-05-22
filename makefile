b6013ada85c41f0b63444f8e337ee9b306547fe4
Julius Pedersen <deifyed@ctemplar.com>
2020-05-22 16:11:21 +0200

    Add minio for object storage

--- makefile
+++ makefile
@@ -22,16 +22,26 @@ release: build push-image
 	@echo "🚀 Release successfully built. We are ready to deploy"
 
 run: start-dependencies
-	mkdir -p static
 	npm run serve
 
 start-dependencies:
-	@docker-compose up -d
+	@COMPOSE_PROJECT_NAME=motoblog docker-compose up -d
 
 clean:
 	@rm -rf node_modules
 	@rm -rf static
 
 create-dotenv-file:
+	$(eval LOCALHOST := $(or $(shell ip addr show docker0 | grep -Po 'inet \K[\d.]+'), $(shell echo "host.docker.internal")))
 	@echo "REDIS_URL=redis://localhost:6379" >> .env
 	@echo "LOG_LEVEL=info" >> .env
+	@echo "GATEKEEPER_URL=http://localhost:4554" >> .env
+	@echo "MINIO_URL=http://localhost:9000" >> .env
+	@echo "MINIO_ACCESS_KEY=X4TO2JNMO05VC0B1AMHU" >> .env
+	@echo "MINIO_SECRET_KEY=XPdhQgldd9GsAaBUoJoOvemGxP5JBrADToQqPupa" >> .env
+	@echo "" >> .env
+	@# Docker compose vars
+	@echo "LOCALHOST=$(LOCALHOST)" >> .env
+	@echo "GATEKEEPER_DISCOVERY_URL=" >> .env
+	@echo "GATEKEEPER_CLIENT_ID=" >> .env
+	@echo "GATEKEEPER_CLIENT_SECRET=" >> .env
--- source/controllers/media.js
+++ source/controllers/media.js
@@ -1,24 +1,60 @@
 const express = require('express')
 const multer = require('multer')
 const { nanoid } = require('nanoid')
+const Minio = require('minio')
 
 const router = express.Router()
 
-const UPLOAD_DIR = 'static'
-
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, UPLOAD_DIR)
-	},
-	filename: (req, file, cb) => {
-		cb(null, nanoid())
-	}
+const BUCKET_DIR = 'motoblog-post-pictures'
+const BUCKET_POLICY = JSON.stringify({
+	Version: '2012-10-17',
+	Statement: [
+		{
+			Sid: 'PublicRead',
+			Effect: 'Allow',
+			Action: ['s3:GetObject'],
+			Principal: '*',
+			Resource: [`arn:aws:s3:::${BUCKET_DIR}/*`]
+		}
+	]
 })
 
-const uploader = multer({ storage })
+const minioClient = new Minio.Client({
+	endPoint: process.env.MINIO_URL,
+	port: parseInt(process.env.MINIO_PORT),
+	useSSL: false,
+	accessKey: process.env.MINIO_ACCESS_KEY,
+	secretKey: process.env.MINIO_SECRET_KEY
+})
 
-router.post('/', uploader.single('picture'), (req, res) => {
-	res.json({ id: req.file.filename }).end()
+async function ensureBucket(bucket) {
+	const exists = await minioClient.bucketExists(bucket)
+
+	if (exists) return
+
+	await minioClient.makeBucket(bucket)
+	await minioClient.setBucketPolicy(BUCKET_DIR, BUCKET_POLICY)
+}
+
+async function storeImage(mimetype, file) {
+	await ensureBucket(BUCKET_DIR)
+	const id = nanoid()
+
+	const metadata = {
+		'Content-Type': mimetype
+	}
+
+	await minioClient.putObject(BUCKET_DIR, id, file, metadata)
+
+	return id
+}
+
+const uploader = multer({ storage: multer.memoryStorage() })
+
+router.post('/', uploader.single('picture'), async (req, res) => {
+	const id = await storeImage(req.file.mimetype, req.file.buffer)
+
+	res.json({ id }).end()
 })
 
 module.exports = router
